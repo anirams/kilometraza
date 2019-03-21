a03090b3c034de2c0c320ff233f48d8363092f68
anirams <marina.stvoric1@gmail.com>
2019-03-21 18:24:49 +0100

    km

---
+++ PracenjeKilometrazeVozila/KilometrazaClasses/KilometrazaClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracenjeKilometrazeVozila.KilometrazaClasses
+{
+    class KilometrazaClass
+    {
+        public int Id { get; set; }
+        public string RegOznaka { get; set; }
+        public DateTime DatumIVrijeme { get; set; }
+        public int PocetnaKilometraza { get; set; }
+        public int ZavrsnaKilometraza { get; set; }
+        public int PrijedjeniKm { get; set; }
+
+        static string connString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        //-----------------------------------------------uzimanje podataka iz baze (pretrazivanje forma)-----------------------------------
+        public DataTable Select(string regOznakaString, DateTime vrijemeOd, DateTime vrijemeDo)
+        {
+            SqlConnection conn = new SqlConnection(connString);
+            DataTable dt = new DataTable();
+            try
+            {
+                string sql = "SELECT * FROM Kilometrazas WHERE RegOznaka=@RegOznaka AND DatumIVrijeme<=@vrijemeDo AND DatumIVrijeme>=@vrijemeOd";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@RegOznaka", regOznakaString);
+                cmd.Parameters.AddWithValue("@vrijemeOd", vrijemeOd);
+                cmd.Parameters.AddWithValue("@vrijemeDo", vrijemeDo);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return dt;
+        }
+
+        // --------------------------------------------------------------UMETANJE NOVOG ROW-A U BAZU,VALIDACIJA---------------------------------
+
+        public bool Insert(KilometrazaClass kilometraza)
+        {
+            bool isSuccess = false;
+            SqlConnection conn = new SqlConnection(connString);
+
+            //-------------------------------------------validacija (FORMAT REG. OZNAKE I kilometraze) ------------------------------------------------- 
+            if (kilometraza.ZavrsnaKilometraza < kilometraza.PocetnaKilometraza || 
+                kilometraza.RegOznaka == "^[a-zA-Z]{2}[0-9]{3,4}[a-zA-Z]{1,2}$")
+            {
+                return isSuccess;
+            }
+
+            try
+            {
+                string sql = "INSERT INTO Kilometrazas (RegOznaka, DatumIVrijeme, PocetnaKilometraza, ZavrsnaKilometraza, PrijedjeniKm) VALUES (@RegOznaka, @DatumIVrijeme, @PocetnaKilometraza, @ZavrsnaKilometraza, @PrijedjeniKm)";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@RegOznaka", kilometraza.RegOznaka);
+                cmd.Parameters.AddWithValue("@DatumIvrijeme", kilometraza.DatumIVrijeme);
+                cmd.Parameters.AddWithValue("@PocetnaKilometraza", kilometraza.PocetnaKilometraza);
+                cmd.Parameters.AddWithValue("@ZavrsnaKilometraza", kilometraza.ZavrsnaKilometraza);
+                cmd.Parameters.AddWithValue("@PrijedjeniKm", kilometraza.PrijedjeniKm);
+                conn.Open();
+                int rows = cmd.ExecuteNonQuery();
+                if (rows > 0)
+                {
+                    isSuccess = true;
+                }
+                else
+                {
+                    isSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return isSuccess;
+        }
+    }
+}
--- PracenjeKilometrazeVozila/MainWindow.xaml.cs
+++ PracenjeKilometrazeVozila/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using PracenjeKilometrazeVozila.KilometrazaClasses;
+using System.Data;
 
 namespace PracenjeKilometrazeVozila
 {
@@ -23,6 +27,105 @@ namespace PracenjeKilometrazeVozila
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        static string connString = ConfigurationManager.ConnectionStrings["connectionString"].ConnectionString;
+
+        KilometrazaClass k = new KilometrazaClass();
+
+        //---------------------------upisivanje forma, validacija-------------------------------------------------------------------------------
+
+        private void UpisKilometrazeButton_Click(object sender, RoutedEventArgs e)
+        {
+            k.RegOznaka = regOznakaTextBox.Text;
+            k.DatumIVrijeme = DateTime.Parse(datumPocetkaTextBox.Text);
+            k.PocetnaKilometraza = Int32.Parse(pocetnaKilometrazaTextBox.Text);
+            k.ZavrsnaKilometraza = Int32.Parse(zavrsnaKilometrazaTextBox.Text);
+            k.PrijedjeniKm = Int32.Parse(zavrsnaKilometrazaTextBox.Text) - Int32.Parse(pocetnaKilometrazaTextBox.Text);
+
+            SqlConnection conn = new SqlConnection(connString);
+            DataTable dt = new DataTable();
+
+            //--------------------------validacija- upis pocetne kilometraze za vozilo za koje je vec upisivana kilometraza-------------------
+            try
+            {
+                
+                string sql = "SELECT ZavrsnaKilometraza FROM Kilometrazas WHERE RegOznaka=@RegOznaka";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                cmd.Parameters.AddWithValue("@RegOznaka", k.RegOznaka);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                conn.Open();
+                adapter.Fill(dt);  
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            int kilometraza = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                if (dt.Rows[i].Field<int>("ZavrsnaKilometraza") > kilometraza)
+                {
+                    kilometraza = dt.Rows[i].Field<int>("ZavrsnaKilometraza");
+                }
+            }
+
+            if (k.ZavrsnaKilometraza < kilometraza)
+            {
+                MessageBox.Show("Posljednja unesena kilometraza za navedeno \n vozilo je bila " + kilometraza
+                    + "\n te početna kilometraža ne smije biti manja od toga!" +
+                    "\n ispravite unos!");
+
+            }
+
+            //-------------------------------------------------------------------------------------------------------------------------
+            else
+            {
+                bool success = k.Insert(k);
+                if (success == true)
+                {
+                    MessageBox.Show("Uspješan unos!");
+                }
+                else
+                {
+                    MessageBox.Show("Došlo je do pogreške! \n Završna kilometraža ne smije biti manja od početne! " +
+                        "\n Registarska oznaka mora biti u formatu ab123cd, ab1234cd, ab1234c ili ab123c");
+                }
+                regOznakaTextBox.Text = "";
+                datumPocetkaTextBox.Text = "";
+                pocetnaKilometrazaTextBox.Text = "";
+                zavrsnaKilometrazaTextBox.Text = "";
+
+            }
+        }
+
+        //-------------------------------------- pretrazivanje forma -------------------------------------------------------
+
+        private void PretragaButton_Click(object sender, RoutedEventArgs e)
+        {
+            string regOznakaString = pretragaRegOznakaTextBox.Text;
+            DateTime vrijemeOd = DateTime.Parse(pretragaPocetniDatumTextBox.Text);
+            DateTime vrijemeDo = DateTime.Parse(pretragaZavrsniDatumTextBox.Text);
+            
+            DataTable dt = k.Select(regOznakaString, vrijemeOd, vrijemeDo);
+            podaciDataGrid.DataContext = dt.DefaultView;
+            int ukupnoKm = 0;
+            for (int i = 0; i < dt.Rows.Count; i++)
+            {
+                ukupnoKm += dt.Rows[i].Field<int>("PrijedjeniKm");
+            }
+            ukupnoKmTextBlock.Text = "Vozilo je prešlo ukupno " + ukupnoKm.ToString() + " u zadanom razdoblju!";
+
+            regOznakaTextBox.Text = "";
+            pretragaPocetniDatumTextBox.Text = "";
+            pretragaZavrsniDatumTextBox.Text = "";
         }
     }
 }
